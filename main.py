b7ea6db223c099f930a1a37e48d17d1d8ebb3e1e
Deepinder Singh Brar <deepindersinghdeep03@gmail.com>
2025-02-19 00:15:58 +0530

    no any changes, just removed caption extraction
    need to get a better way to extract caption

--- main.py
+++ main.py
@@ -46,25 +46,36 @@ def main_func():
             try:
                 print('finding more')
                 time.sleep(1)
-                page.click('span.xlej3yl.x1rg5ohu.xdl72j9.x1c4vz4f.x2lah0s.xsgj6o6',timeout=1000)
-                # exists = False
-                # try:
-                #     page.locator('span.xlej3yl.x1rg5ohu.xdl72j9.x1c4vz4f.x2lah0s.xsgj6o6').is_visible(timeout=100)
-                #     exists = True
-                #     print("More button found")
-                # except:
-                #     exists = False
-                #     print("NOT FOUND!!")
+                # page.click('span.xlej3yl.x1rg5ohu.xdl72j9.x1c4vz4f.x2lah0s.xsgj6o6',timeout=1000)
+                # page.click(f'span.x6ikm8r.x10wlt62.xuxw1ft:nth-of-type({reel_index})')
 
-                # if exists:
-                #     page.click('span.xlej3yl.x1rg5ohu.xdl72j9.x1c4vz4f.x2lah0s.xsgj6o6')
-                #     print('clicked more')
+                # div_content = page.locator('div.x78zum5.xl56j7k.x1n2onr6.xh8yej3').nth(reel_index)
+                
+                # div_content = page.locator('div.x1qjc9v5.x9f619.x78zum5,xg7h5cd.xl56j7k.x1xfsgkm.xqmdsaz.x1bhewko.xgv127d.xh8yej3').nth(reel_index) 
+                # print('found div_content')
+
+                # div_content.locator('span.x6ikm8r.x10wlt62.xuxw1ft').first().click()
+                # here the div_content contains the whole reel but each time i try to click the more button it won't work until the window is in focus, i tried copying the whole page content but that does not help
+
+                # print('text - \n',div_content.inner_text())
+                # input('continue?')
+
+                # div_content.locator(f'span.x6ikm8r.x10wlt62.xuxw1ft:nth-of-type({reel_index})').first.click()
+                # print('clicked more')
+
+                
 
                 time.sleep(0.5)
                 # print('inner text - ')
                 # print(page.inner_text('div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1emribx.x1uhb9sk.x1iyjqo2.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1.x2lwn1j.xeuugli.x6ikm8r.x10wlt62.x1d8287x.xrok2fi.xz4gly6'))
 
-                inner_html = page.inner_html(f'div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1emribx.x1uhb9sk.x1iyjqo2.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1:nth-of-type({reel_index})')
+                # inner_html = page.inner_html(f'div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1emribx.x1uhb9sk.x1iyjqo2.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1:nth-of-type({reel_index})')
+
+                inner_html = page.locator('div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1emribx.x1uhb9sk.x1iyjqo2.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1').inner_html()
+
+                print('inner html - ',inner_html)
+                input('continue?')
+
                 # print("INNER HTML - ", inner_html)
                 parsed_caption = parse_caption_tag(inner_html)
                 print(f'Parsed html = \n{parsed_caption}')
@@ -78,11 +89,12 @@ def main_func():
                 
 
 
-                page.keyboard.press('ArrowDown')
                 input('continue?')
+                page.keyboard.press('ArrowDown')
                 time.sleep(1)
             except Exception as e:
                 print(e)
+                reel_index += 1
                 page.keyboard.press('ArrowDown')
                 time.sleep(1)
         input('close?')
